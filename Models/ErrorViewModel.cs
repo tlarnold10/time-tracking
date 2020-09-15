df27da409dc8dc4baad58205b174bba2445fcf25
tlarnold10 <tlarnold10@hotmail.com>
2020-09-14 21:27:12 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeTrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
